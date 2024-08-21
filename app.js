91a637c579043a4353a108f1c356c9bd1e367863
Daviluca05 <125285845+Daviluca05@users.noreply.github.com>
2024-08-21 19:37:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-// URL da API The Dog API para buscar uma imagem aleatória
 const apiURL = 'https://api.thedogapi.com/v1/images/search';
 
-// Função para buscar uma imagem de cão
 async function fetchDogImage() {
     try {
         const response = await fetch(apiURL);
         const data = await response.json();
         const dogImageUrl = data[0].url;
 
-        // Exibe a imagem no DOM
         document.getElementById('dog-image').innerHTML = `<img src="${dogImageUrl}" alt="Imagem de um cão">`;
 
     } catch (error) {
@@ -16,5 +13,4 @@ async function fetchDogImage() {
     }
 }
 
-// Chama a função ao carregar a página para exibir a primeira imagem
 fetchDogImage();
